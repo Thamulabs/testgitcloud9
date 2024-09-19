2b437489c359bccb4b40b5c3504d7cff1646d1f4
nivedita-atci <69342458+nivedita-atci@users.noreply.github.com>
2024-09-19 15:09:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 def hello_world():
-    return "Hello, ServiceNow DevOps team! added change model!"
+    return "Hello, ServiceNow DevOps! added change model!"
 
 if __name__ == "__main__":
     print(hello_world())
